a23202a514ebf065055fb4d2be453d3b5e758c2c
Mohd-Saddam <50014573+Mohd-Saddam@users.noreply.github.com>
2024-01-12 00:00:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "Testing Jenkins with webhook for githubs"
+    return jsonify("Testing Jenkins with webhook for githubs")
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
