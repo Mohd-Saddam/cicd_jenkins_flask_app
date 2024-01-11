216194ac7704ee65b8b538a62bcb9f27d5e06f8f
Mohd-Saddam <msaddam2786@gmail.com>
2024-01-11 23:02:03 +0530

    testwebhook

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "Testing Jenkins with webhook"
+    return "Testing Jenkins with webhook for github"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
